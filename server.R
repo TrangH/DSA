5a6ea90c8d54daacb7d8a0a21adbfbdfd44425d2
Long Vo <30339577+longhaivo@users.noreply.github.com>
2019-05-21 07:29:53 +0800

    Update server.R

--- server.R
+++ server.R
@@ -42,11 +42,11 @@ shinyServer(function(input, output) {
     } else {
       ###Load all data before the plot
       ppp_ken <- read.csv(input$file1$datapath,
-                     header = input$header,sep = input$sep,quote = input$quote)
+                          header = input$header,sep = input$sep,quote = input$quote)
       exp_ken <- read.csv(input$file2$datapath,
                           header = input$header,sep = input$sep,quote = input$quote)
       bs_ken <- read.csv(input$file3$datapath,
-                          header = input$header,sep = input$sep,quote = input$quote)
+                         header = input$header,sep = input$sep,quote = input$quote)
       ###Per capita real income ($)----
       q_ken=exp_ken/ppp_ken #per capita consumption = real income 
       #sort countries (column) by real income p.c----
@@ -54,14 +54,65 @@ shinyServer(function(input, output) {
       q_ken1<-q_ken[,match(csort,colnames(q_ken))]
       ppp_ken1<-ppp_ken[,match(csort,colnames(ppp_ken))]
       bs_ken1<-bs_ken[,match(csort,colnames(bs_ken))]
-      
-      
-      #
-      #p_sd<-100*sqrt(as.numeric(sigma2_P_mat[upper.tri(sigma2_P_mat,diag=FALSE)]))
-      #q_sd<-100*sqrt(as.numeric(sigma2_mat[upper.tri(sigma2_mat,diag=FALSE)]))
-    }
+      }
   })
   
+  ###The q dispersion matrix----
+  output$q_disp <- renderTable({
+    if (is.null(input$file1)| is.null(input$file2)| is.null(input$file3)){
+      return(NULL)
+    } else {
+      #compute the logExp_cd and sigma2_cd matrices----
+      #[sigma is the bivariate variance matrix]
+      logq_list=list();bs_list=list(); 
+      logQ_mat=NULL;sigma2_mat=NULL;
+      for (c in 1:ncol(q_ken1)){
+        #logq_icd and w_icd
+        logq_mat=NULL;bs_mat=NULL;
+        for (d in 1:ncol(q_ken1)){
+          logq_icd=log(q_ken1[,c])-log(q_ken1[,d])
+          logq_mat=cbind(logq_mat,logq_icd);
+          #
+          bs_icd=(0.5*(bs_ken1[,c]+bs_ken1[,d]))/100 
+          bs_mat=cbind(bs_mat,bs_icd);
+        }
+        rownames(logq_mat)=rownames(bs_mat)=rownames(q_ken1);
+        colnames(logq_mat)=colnames(bs_mat)=colnames(q_ken1);
+        #
+        logq_list[[c]]=logq_mat; names(logq_list)[c]=colnames(q_ken1)[c];
+        #budget share matrix
+        bs_list[[c]]=bs_mat; names(bs_list)[c]=colnames(q_ken1)[c];
+        #logq_cd matrix
+        logQ_cd=colSums(logq_mat*bs_mat); 
+        logQ_mat=rbind(logQ_mat,logQ_cd);
+        #sigma2_cd matrix
+        sigma2_cd=colSums(bs_mat*sweep(logq_mat,2,logQ_cd,FUN='-')^2) #logq_cd does not change across items
+        sigma2_mat<-rbind(sigma2_mat,sigma2_cd)
+        #round(colSums(bs_mat*(sweep(logq_mat,2,logq_cd,FUN='-'))),2) #Check: all zero
+        #round(colSums(bs_mat),2) #Check: all 1
+      }
+      colnames(logQ_mat)=rownames(logQ_mat)=colnames(q_ken1)
+      colnames(sigma2_mat)=rownames(sigma2_mat)=colnames(q_ken1)
+      
+      #[among quintiles]
+      c_q1<-csort[1:35];c_q2<-csort[36:71];c_q3<-csort[72:107];c_q4<-csort[108:143];c_q5<-csort[144:176]
+      c_qlist<-list(c_q1,c_q2,c_q3,c_q4,c_q5)
+      #
+      sigma_mat<-NULL;
+      for (i in 1:5){
+        rowpos<-match(c_qlist[[i]],rownames(sigma2_mat))
+        vec=NULL;
+        for (j in 1:5) {
+          colpos<-match(c_qlist[[j]],colnames(sigma2_mat))
+          vec<-c(vec,mean(sqrt(sigma2_mat[rowpos,colpos])))
+        }
+        sigma_mat<-rbind(sigma_mat,vec)
+      }
+      colnames(sigma_mat)=rownames(sigma_mat)=c('Q1 (Richest)','Q2','Q3','Q4','Q5 (Poorest)')
+      return(sigma_mat)}
+  })
+  
+  ###The P vs Q dispersion plot----
   output$newplot <- renderPlot(
     if (is.null(input$file1)| is.null(input$file2)| is.null(input$file3)){
       return(NULL)
@@ -128,7 +179,7 @@ shinyServer(function(input, output) {
       #dispersion measure  
       p_sd<-100*sqrt(as.numeric(sigma2_P_mat[upper.tri(sigma2_P_mat,diag=FALSE)]))
       q_sd<-100*sqrt(as.numeric(sigma2_mat[upper.tri(sigma2_mat,diag=FALSE)]))
-    ###ggplot contour 
+      ###ggplot contour 
       library(ggplot2)
       commonTheme = list(labs(color="",fill="",
                               x="",
@@ -166,7 +217,7 @@ shinyServer(function(input, output) {
         guides(alpha="none") +
         commonTheme +
         labs(x='Prices', y='Quantities')
-    #plot(sigma2_mat[,1],sigma2_P_mat[,2])
+      #plot(sigma2_mat[,1],sigma2_P_mat[,2])
     }
   )
 })
